23ec7a82be7672d9ddb3969f3a4cf122ff8f51ab
Kamil Cholewa <kcholew4@gmail.com>
2023-03-19 20:10:04 +0100

    Enable cors

--- server/app.py
+++ server/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from resources.product import Product
 from resources.products_list import ProductsList
 
 app = Flask(__name__)
 api = Api(app)
-
+CORS(app)
 
 api.add_resource(Product, "/api/products/<id>")
 api.add_resource(ProductsList, "/api/products")

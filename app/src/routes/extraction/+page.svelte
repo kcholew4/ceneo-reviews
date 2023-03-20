713accf6df13528edd8932e2e188ae3c5a96e0f4
Kamil Cholewa <kcholew4@gmail.com>
2023-03-20 10:51:44 +0100

    Show not found notification

--- app/src/routes/extraction/+page.svelte
+++ app/src/routes/extraction/+page.svelte
@@ -1,16 +1,52 @@
 <script>
+	import { env } from '$env/dynamic/public';
 	import { goto } from '$app/navigation';
-	import { TextInput, Button, FormGroup, Form } from 'carbon-components-svelte';
+	import { TextInput, Button, FormGroup, Form, ToastNotification } from 'carbon-components-svelte';
 
 	let id = null;
+
+	const productExists = async (id) => {
+		const url = `http://${env.PUBLIC_API_URL}/api/products/${id}/check`;
+
+		const res = await fetch(url);
+
+		if (res.status == 200) {
+			return true;
+		}
+
+		return false;
+	};
+
+	let showNotFound = false;
 </script>
+
+{#if showNotFound}
+	<div class="alert">
+		<ToastNotification
+			lowContrast
+			kind="error"
+			title="Błąd"
+			subtitle="Nie znaleziono produktu o podanym ID."
+			on:close={() => {
+				showNotFound = false;
+			}}
+		/>
+	</div>
+{/if}
 
 <div class="container">
 	<h1>Wyszukaj produkt</h1>
 	<p>Pobierz opinie o produkcie z portalu ceneo.pl wpisując jego id.</p>
 	<Form
-		on:submit={(e) => {
+		on:submit={async (e) => {
 			e.preventDefault();
+
+			if (!(await productExists(id))) {
+				console.log('product does not exists');
+				showNotFound = true;
+				return;
+			}
+
 			goto(`/products/${id}`);
 		}}
 	>
@@ -33,5 +69,10 @@
 
 	p {
 		margin-bottom: 1em;
+	}
+
+	.alert {
+		position: fixed;
+		right: 0;
 	}
 </style>

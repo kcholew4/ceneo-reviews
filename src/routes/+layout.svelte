28e35ad8e7ced431e2331a09de8ddb83039ff6f9
Kamil Cholewa <kcholew4@gmail.com>
2023-03-30 20:35:15 +0200

    Create basic front page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import 'carbon-components-svelte/css/g90.css';
+	import { Content } from 'carbon-components-svelte';
+</script>
+
+<Content>
+	<slot />
+</Content>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Grid, Row, Column } from 'carbon-components-svelte';
+</script>
+
+<Grid>
+	<Row>
+		<Column lg={8}>
+			<h1>Pobierz opinie z portalu Ceneo.pl</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo aliquid ab, laudantium
+				sequi nulla corrupti quasi esse officiis non provident molestias praesentium in maiores
+				vitae distinctio autem nisi quod harum.
+			</p>
+		</Column>
+	</Row>
+</Grid>
+
+<style>
+	h1 {
+		margin-bottom: 0.5em;
+	}
+</style>

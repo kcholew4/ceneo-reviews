dc654738e41f15b59389987c53abe736723cb23b
Kamil Cholewa <kcholew4@gmail.com>
2023-04-09 15:26:24 +0200

    Fix double author bug

--- src/lib/ceneo/review.ts
+++ src/lib/ceneo/review.ts
@@ -1,148 +1,158 @@
-import type { Cheerio, AnyNode } from "cheerio";
+import type { Cheerio, AnyNode } from 'cheerio';
 
 export class Review {
-  private static SELECTORS = {
-    author: ".user-post__author-name",
-    recommendation: ".user-post__author-recomendation > em",
-    score_count: ".user-post__score-count",
-    verified: ".user-post__info > .review-pz",
-    time_tag: ".user-post__published > time",
-    votes_yes: ".vote-yes > span",
-    votes_no: ".vote-no > span",
-    text: ".user-post__text",
-    review_feature: ".review-feature",
-    review_feature_col: ".review-feature__col",
-    review_feature_item: ".review-feature__item"
-  }
+	private static SELECTORS = {
+		author: '.user-post__author-name',
+		recommendation: '.user-post__author-recomendation > em',
+		score_count: '.user-post__score-count',
+		verified: '.user-post__info > .review-pz',
+		time_tag: '.user-post__published > time',
+		votes_yes: '.vote-yes > span',
+		votes_no: '.vote-no > span',
+		text: '.user-post__text',
+		review_feature: '.review-feature',
+		review_feature_col: '.review-feature__col',
+		review_feature_item: '.review-feature__item'
+	};
 
-  private $review: Cheerio<AnyNode>;
+	private $review: Cheerio<AnyNode>;
 
-  id: string;
-  author: string;
-  recommendation: string;
-  score: number;
-  verified: boolean;
-  published: Date;
-  bought: Date | null;
-  votesYes: number;
-  votesNo: number;
-  text: string;
-  pros: string[];
-  cons: string[];
+	id: string;
+	author: string;
+	recommendation: string;
+	score: number;
+	verified: boolean;
+	published: Date;
+	bought: Date | null;
+	votesYes: number;
+	votesNo: number;
+	text: string;
+	pros: string[];
+	cons: string[];
 
-  constructor($review: Cheerio<AnyNode>) {
-    this.$review = $review
+	constructor($review: Cheerio<AnyNode>) {
+		this.$review = $review;
 
-    this.id = this.extractId();
-    this.author = this.extractAuthor();
-    this.recommendation = this.extractRecommendation();
-    this.score = this.extractScore();
-    this.verified = this.extractVerified();
-    this.published = this.extractPublished();
-    this.bought = this.extractBought();
-    this.votesYes = this.extractVotesYes();
-    this.votesNo = this.extractVotesNo();
-    this.text = this.extractText();
-    this.pros = this.extractPros();
-    this.cons = this.extractCons();
-  }
+		this.id = this.extractId();
+		this.author = this.extractAuthor();
+		this.recommendation = this.extractRecommendation();
+		this.score = this.extractScore();
+		this.verified = this.extractVerified();
+		this.published = this.extractPublished();
+		this.bought = this.extractBought();
+		this.votesYes = this.extractVotesYes();
+		this.votesNo = this.extractVotesNo();
+		this.text = this.extractText();
+		this.pros = this.extractPros();
+		this.cons = this.extractCons();
+	}
 
-  private getTimeTags() {
-    return this.$review.find(Review.SELECTORS.time_tag)
-  }
+	private getTimeTags() {
+		return this.$review.find(Review.SELECTORS.time_tag);
+	}
 
-  private getReviewFeatureCols() {
-    const reviewFeature = this.$review.find(Review.SELECTORS.review_feature);
+	private getReviewFeatureCols() {
+		const reviewFeature = this.$review.find(Review.SELECTORS.review_feature);
 
-    if (reviewFeature.length === 0) {
-      return null;
-    }
+		if (reviewFeature.length === 0) {
+			return null;
+		}
 
-    return reviewFeature.find(Review.SELECTORS.review_feature_col);
-  }
+		return reviewFeature.find(Review.SELECTORS.review_feature_col);
+	}
 
-  private extractId() {
-    return this.$review.attr("data-entry-id") ?? "";
-  }
+	private extractId() {
+		return this.$review.attr('data-entry-id') ?? '';
+	}
 
-  private extractAuthor() {
-    return this.$review.find(Review.SELECTORS.author).text().trim();
-  }
+	private extractAuthor() {
+		return this.$review.find(Review.SELECTORS.author).first().text().trim();
+	}
 
-  private extractRecommendation() {
-    return this.$review.find(Review.SELECTORS.recommendation).text().trim();
-  }
+	private extractRecommendation() {
+		return this.$review.find(Review.SELECTORS.recommendation).text().trim();
+	}
 
-  private extractScore() {
-    const raw = this.$review.find(Review.SELECTORS.score_count).text();
-    const score = raw.match(/(?<score>.+)(?=\/\d)/)?.groups?.score;
+	private extractScore() {
+		const raw = this.$review.find(Review.SELECTORS.score_count).text();
+		const score = raw.match(/(?<score>.+)(?=\/\d)/)?.groups?.score;
 
-    if (!score) {
-      return 0;
-    }
+		if (!score) {
+			return 0;
+		}
 
-    return parseFloat(score.replaceAll(",", "."));
-  }
+		return parseFloat(score.replaceAll(',', '.'));
+	}
 
-  private extractVerified() {
-    return this.$review.is(Review.SELECTORS.verified);
-  }
+	private extractVerified() {
+		return this.$review.is(Review.SELECTORS.verified);
+	}
 
-  private extractPublished() {
-    const timeTags = this.getTimeTags();
-    return new Date(timeTags.first().attr("datetime") ?? 0)
-  }
+	private extractPublished() {
+		const timeTags = this.getTimeTags();
+		return new Date(timeTags.first().attr('datetime') ?? 0);
+	}
 
-  private extractBought() {
-    const timeTags = this.getTimeTags();
+	private extractBought() {
+		const timeTags = this.getTimeTags();
 
-    if (timeTags.length < 2) {
-      return null;
-    }
+		if (timeTags.length < 2) {
+			return null;
+		}
 
-    return new Date(timeTags.last().attr("datetime") ?? 0)
-  }
+		return new Date(timeTags.last().attr('datetime') ?? 0);
+	}
 
-  private extractVotesYes() {
-    return parseInt(this.$review.find(Review.SELECTORS.votes_yes).text()) 
-  }
+	private extractVotesYes() {
+		return parseInt(this.$review.find(Review.SELECTORS.votes_yes).text());
+	}
 
-  private extractVotesNo() {
-    return parseInt(this.$review.find(Review.SELECTORS.votes_no).text());
-  }
+	private extractVotesNo() {
+		return parseInt(this.$review.find(Review.SELECTORS.votes_no).text());
+	}
 
-  private extractText() {
-    // TODO: More advanced text extraction
-    return this.$review.find(Review.SELECTORS.text).text().trim();
-  }
+	private extractText() {
+		// TODO: More advanced text extraction
+		return this.$review.find(Review.SELECTORS.text).text().trim();
+	}
 
-  // TODO: Don't use string manipulation to extract pros and cons
-  private extractPros() {
-    const featureCols = this.getReviewFeatureCols();
+	// TODO: Don't use string manipulation to extract pros and cons
+	private extractPros() {
+		const featureCols = this.getReviewFeatureCols();
 
-    if (!featureCols) {
-      return [];
-    }
+		if (!featureCols) {
+			return [];
+		}
 
-    const prosCol = featureCols.first();
+		const prosCol = featureCols.first();
 
-    //Maybe not the best solution...
-    return prosCol.text().trim().split("\n").slice(1).map((item) => item.trim());
-  }
+		//Maybe not the best solution...
+		return prosCol
+			.text()
+			.trim()
+			.split('\n')
+			.slice(1)
+			.map((item) => item.trim());
+	}
 
-  private extractCons() {
-    const featureCols = this.getReviewFeatureCols();
+	private extractCons() {
+		const featureCols = this.getReviewFeatureCols();
 
-    if (!featureCols) {
-      return [];
-    }
+		if (!featureCols) {
+			return [];
+		}
 
-    if (featureCols.length < 2) {
-      return [];
-    }
+		if (featureCols.length < 2) {
+			return [];
+		}
 
-    const consCol = featureCols.last();
+		const consCol = featureCols.last();
 
-    return consCol.text().trim().split("\n").slice(1).map((item) => item.trim())
-  }
+		return consCol
+			.text()
+			.trim()
+			.split('\n')
+			.slice(1)
+			.map((item) => item.trim());
+	}
 }

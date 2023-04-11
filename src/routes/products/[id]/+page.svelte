ff6ca19cf5a28085637b12c03850a445f9b44b92
Kamil Cholewa <kcholew4@gmail.com>
2023-04-11 20:26:33 +0200

    Move filter controls to seperate component

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -10,8 +10,7 @@
     Dropdown,
     MultiSelect
   } from 'carbon-components-svelte';
-  import { ReviewsProperties } from '$lib/helpers/reviewsProperties';
-  import { ReviewsFilters, type FilterFunction } from '$lib/helpers/reviewsFilters';
+  import Filters from './Filters.svelte';
   import type { Review } from '@prisma/client';
 
   export let data;
@@ -42,25 +41,7 @@
     return new Intl.DateTimeFormat().format(date);
   };
 
-  const reviewsFilters = new ReviewsFilters(reviews);
-  const reviewsProperties = new ReviewsProperties(reviews);
-
-  const recommendations = reviewsProperties.recommendations();
-  const verifiedOptions = reviewsProperties.verifiedOptions();
-  const scores = reviewsProperties.scores();
-  const pros = reviewsProperties.pros();
-  const cons = reviewsProperties.cons();
-
-  let rows: ReturnType<typeof reviewsToRows>;
-
-  let filters: {
-    [index: string]: FilterFunction;
-  } = {};
-
-  $: {
-    const displayedReviews = reviewsFilters.apply(Object.values(filters));
-    rows = reviewsToRows(displayedReviews);
-  }
+  let rows = reviewsToRows(reviews);
 </script>
 
 <Modal bind:open={modalOpen} bind:modalHeading passiveModal>{modalContent}</Modal>
@@ -71,117 +52,14 @@
       <h1>{data.product.name}</h1>
     </Column>
   </Row>
-  <Row padding>
-    <Column lg={4}>
-      <Dropdown
-        titleText="Rekomendacja"
-        selectedId={-1}
-        items={[
-          { id: -1, text: 'Wszystkie' },
-          ...recommendations.map((value, index) => ({
-            id: index,
-            text: value ? value : 'Brak'
-          }))
-        ]}
-        on:select={(event) => {
-          const id = event.detail.selectedId;
-
-          if (id < 0) {
-            delete filters.recommendation;
-            return (filters = filters);
-          }
-
-          filters.recommendation = ReviewsFilters.createRecommendationFilter(recommendations[id]);
-        }}
-      />
-    </Column>
-    <Column lg={4}>
-      <Dropdown
-        titleText="Potwierdzona zakupen"
-        selectedId={-1}
-        items={[
-          { id: -1, text: 'Wszystkie' },
-          ...verifiedOptions.map((value, index) => ({
-            id: index,
-            text: value ? 'Tak' : 'Nie'
-          }))
-        ]}
-        on:select={(event) => {
-          const id = event.detail.selectedId;
-
-          if (id < 0) {
-            delete filters.verified;
-            return (filters = filters);
-          }
-
-          filters.verified = ReviewsFilters.createVerifiedFilter(verifiedOptions[id]);
-        }}
-      />
-    </Column>
-    <Column lg={4}>
-      <MultiSelect
-        titleText="Ocena"
-        label="Wybierz oceny..."
-        items={scores.map((value, index) => ({
-          id: index,
-          text: value.toString()
-        }))}
-        on:select={(event) => {
-          if (event.detail.selectedIds.length === 0) {
-            delete filters.scores;
-            return (filters = filters);
-          }
-
-          filters.scores = ReviewsFilters.createScoresFilter(
-            event.detail.selected.map((single) => parseFloat(single.text))
-          );
-        }}
-      />
-    </Column>
-  </Row>
-  <Row padding>
-    <Column lg={6}>
-      <MultiSelect
-        titleText="Zalety"
-        label="Wybierz zalety..."
-        items={pros.map((value, index) => ({
-          id: index,
-          text: value
-        }))}
-        on:select={(event) => {
-          if (event.detail.selectedIds.length === 0) {
-            delete filters.pros;
-            return (filters = filters);
-          }
-
-          filters.pros = ReviewsFilters.createProsFilter(
-            event.detail.selected.map(({ text }) => text)
-          );
-        }}
-      />
-    </Column>
-    <Column lg={6}>
-      <MultiSelect
-        titleText="Wady"
-        label="Wybierz zalety..."
-        items={cons.map((value, index) => ({
-          id: index,
-          text: value
-        }))}
-        on:select={(event) => {
-          if (event.detail.selectedIds.length === 0) {
-            delete filters.cons;
-            return (filters = filters);
-          }
-
-          filters.cons = ReviewsFilters.createConsFilter(
-            event.detail.selected.map(({ text }) => text)
-          );
-        }}
-      />
-    </Column>
-  </Row>
 </Grid>
+
+<Filters
+  {reviews}
+  on:filter={(event) => {
+    rows = reviewsToRows(event.detail.reviews);
+  }}
+/>
 
 <div class="table-container">
   <Grid>
